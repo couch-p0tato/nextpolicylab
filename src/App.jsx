2b071475713e49709af3e11b60fbb529c07813dc
Moyo <moyoadeniji144@gmail.com>
2025-07-16 14:13:06 +0100

    n+2  Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Hero from './components/Hero'
 import Statement from './components/Statement'
 import Projects from './components/Projects'
@@ -6,18 +6,43 @@ import Team from './components/Team'
 import Testimonial from './components/Testimonial'
 import Footer from './components/Footer'
 import Impact from './components/Impact'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const App = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+   useEffect(() => {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 6000);
+
+      return () => clearTimeout(timer);
+    }, []);
+
   return (
-    <div className='main-font overflow-hidden'>
-      <Hero />
-      <Statement />
-      <Impact />
-      <Projects />
-      <Team />
-      <Testimonial />
-      <Footer />
-    </div>
+    <>
+      {isLoading ? (
+        <div className={`fixed inset-0 bg-white flex justify-center items-center z-50 transition-opacity duration-500 ${isLoading ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+          <DotLottieReact
+            src="https://lottie.host/86312fde-a5fb-4064-ba6b-b0faf02c244d/J4NalnfRaA.lottie"
+            loop
+            autoplay
+            style={{ height: '300px', width: '300px' }}
+          />
+        </div>
+      ) : (
+        <div className='main-font overflow-hidden'>
+          <Hero />
+          <Statement />
+          <Impact />
+          <Projects />
+          <Team />
+          <Testimonial />
+          <Footer />
+        </div>
+      )}
+    </>
   )
 }
 
